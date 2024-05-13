d43adefda55ea504a42f0822065c14e52cd20898
alatkar <alatkar@yahoo.com>
2024-05-13 12:46:13 -0700

    Carousal, new screens

--- src/components/home/home-page.tsx
+++ src/components/home/home-page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 // import { lusitana } from '@/app/ui/fonts';
@@ -7,10 +8,26 @@ import { Navbar } from '../navbar/navbar';
 import { LoginButtonComp } from '../login/login-button';
 import { Button } from '../ui/button';
 
+import Autoplay from "embla-carousel-autoplay"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import useEmblaCarousel from 'embla-carousel-react';
+import React from 'react';
+
+
 export default function HomePage() {
+  const plugin = React.useRef(
+    Autoplay({ delay: 5000, stopOnInteraction: true })
+  )
+
   return (
-    <div className="mt-2 flex grow flex-col gap-2 md:flex-row">
-      <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 md:w-2/5 md:px-20">
+    <div className="mt-2 flex grow flex-col gap-1 md:flex-row">
+      <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 md:w-2/6 md:px-20">
         <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
           <>Welcome to PX iO.</> {' '}
           <a href="https://phoenixio.one/" className="text-blue-500 strong">
@@ -30,12 +47,73 @@ export default function HomePage() {
           <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
         </Link> */}
       </div>
-      <div className="flex">
-        <div className="flex flex-col grow items-center justify-center p-6 rounded-lg bg-gray-50  md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+      <div className="flex flex-col grow items-center justify-center bg-gray-50 ">
+        <Carousel className="items-center justify-center w-full max-w-5xl"
+          opts={{
+            align: "start",
+            loop: true,
+          }}
+          plugins={[plugin.current]}
+          >
+          <CarouselContent className="-ml-1">            
+            <CarouselItem>
+              <Image
+                src="/focus2.png"
+                width={1000}
+                height={460}
+                className="hidden md:block"
+                alt="Phoenix iO Logo of Desktop Version"
+                priority
+              />
+            </CarouselItem>
+            <CarouselItem>
+              <Image
+                src="/focus3.png"
+                width={1000}
+                height={460}
+                className="hidden md:block"
+                alt="Phoenix iO Logo of Desktop Version"
+                priority
+              />
+            </CarouselItem>
+            <CarouselItem>
+              <Image
+                src="/focus4.png"
+                width={1000}
+                height={460}
+                className="hidden md:block"
+                alt="Phoenix iO Logo of Desktop Version"
+                priority
+              />
+            </CarouselItem>
+            <CarouselItem>
+              <Image
+                src="/focus5.png"
+                width={1000}
+                height={460}
+                className="hidden md:block"
+                alt="Phoenix iO Logo of Desktop Version"
+                priority
+              />
+            </CarouselItem>
+            <CarouselItem>
+              <Image
+                src="/focus6.png"
+                width={1000}
+                height={460}
+                className="hidden md:block"
+                alt="Phoenix iO Logo of Desktop Version"
+                priority
+              />
+            </CarouselItem>
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+        {/* <div className="flex flex-col grow items-center justify-center p-6 rounded-lg bg-gray-50  md:px-28 md:py-12">         
           <div className="">
             <Image
-              src="/pilot.png"
+              src="/focus1.png"
               width={1000}
               height={460}
               className="hidden md:block"
@@ -43,7 +121,7 @@ export default function HomePage() {
               priority
             />
             <Image
-              src="/pilot.png"
+              src="/focus1.png"
               width={360}
               height={420}
               className="block md:hidden"
@@ -58,7 +136,7 @@ export default function HomePage() {
               <span>Contact us</span> <ArrowRightIcon className="w-5 md:w-6" />
             </Link>
           </div>       
-        </div>
+        </div> */}
         {/*<div className="flex ustify-center p-2  bg-gray-50  md:w-3/5 md:px-28 md:py-12">
           <strong>Contact us</strong> {' '}
           <form action="">
