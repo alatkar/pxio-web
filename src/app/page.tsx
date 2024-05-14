02638bbea29aab61efd6559b6b2c5d1c9e7811bd
alatkar <alatkar@yahoo.com>
2024-05-14 11:43:02 -0700

    Visitor IP address

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
 import HomePage from "@/components/home/home-page";
 import Image from "next/image";
 import {auth} from '../../auth'
+import { cookies, headers } from 'next/headers'
 
-export default function Home() {
-  return (
+export async function myfunc() {
+  const forwarded = headers().get("x-forwarded-for")
+  console.log("Visitor IP address", forwarded)
+  /* const ip = forwarded ? forwarded.split(/, /)[0] : Conneconnection.remoteAddress
+  return {
+    props: {
+      ip,
+    },
+  } */
+}
+
+export default async function Home() {
+  await myfunc();
+  return (    
     <>
       {/* <pre>{JSON.stringify(session, null, 2)}</pre> */}
       <HomePage />      
